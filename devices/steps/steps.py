c17f190257fba1c1439b3c2d196518a96e2745d4
Dominik Fuchs <fuchsd@in.tum.de>
2020-11-07 11:37:03 +0100

    listify steps

--- devices/steps/steps.py
+++ devices/steps/steps.py
@@ -14,28 +14,22 @@ LED_INVERT = False    # True to invert the signal (when using NPN transistor lev
 LED_CHANNEL = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
 
 
-class Steps:    
+class Steps(list):
     def __init__(self):
         # Create NeoPixel object with appropriate configuration.
         self.strip = PixelStrip(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
         # Intialize the library (must be called once before other functions).
         self.strip.begin()
 
-        # define steps
-        self.steps = []
-
         for i, led_counter in enumerate(NUMBER_OF_LEDS):
             # get last led
-            start = 0 if i == 0 else (self.steps[-1][-1] + 1)
+            start = 0 if i == 0 else (self[-1][-1] + 1)
             # define step 
             step = Step(self.strip, start, start + led_counter)
-            self.steps.append(step)
+            self.append(step)
     
     def __len__(self):
         return sum(NUMBER_OF_LEDS)
-
-    def __iter__(self):
-        return self.steps.__iter__()
 
     def off(self):
         for step in self.steps:

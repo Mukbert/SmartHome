e0eee2370ae5f3e2ef258865ad5eee8a3683da5e
Dominik Fuchs <fuchsd@in.tum.de>
2020-11-07 11:38:11 +0100

    listify step

--- devices/steps/step.py
+++ devices/steps/step.py
@@ -1,16 +1,13 @@
 from rpi_ws281x import Color
 from devices.steps import util
 
-class Step:
+class Step(list):
     def __init__(self, strip, start, stop):
+        super().__init__(range(start, stop))
         self.strip = strip
-        self.leds = range(start, stop)
-
-    def __iter__(self):
-        return self.leds.__iter__()
-
+    
     def off(self, ms=0):
-        for led in self.leds:
+        for led in self:
             self.strip.setPixelColor(led, Color(0, 0, 0))
         self.strip.show()
         util.wait(ms)

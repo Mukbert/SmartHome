cdfd201fa1d4f4ecbdd5db160aaf8fde3585c068
Dominik Fuchs <fuchsd@in.tum.de>
2020-11-07 22:56:17 +0100

    fixed action filter

--- devices/steps/__init__.py
+++ devices/steps/__init__.py
@@ -6,4 +6,4 @@ def run(action):
     return getattr(steps, action)()
 
 def actions():
-    return list(filter(lambda x: callable(x) and not x.startswith("_") and x not in dir(list()), dir(steps)))
+    return list(filter(lambda x: callable(getattr(steps, x)) and not x.startswith("_") and x not in dir(list()), dir(steps)))

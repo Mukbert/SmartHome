59d4aafe1b2c000207d8c7d033570a7b457090d3
Dominik Fuchs <fuchsd@in.tum.de>
2020-11-07 22:51:49 +0100

    add action filter

--- devices/steps/__init__.py
+++ devices/steps/__init__.py
@@ -6,4 +6,4 @@ def run(action):
     return getattr(steps, action)()
 
 def actions():
-    return list(filter(lambda x: not x.startswith("_"), dir(steps)))
+    return list(filter(lambda x: not x.startswith("_") and x not in dir(list()), dir(steps)))
